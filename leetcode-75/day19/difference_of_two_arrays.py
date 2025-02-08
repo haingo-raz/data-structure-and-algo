3a8ae57b4718d7236947066fa54fe8db1dbc4893
Haingo <100688816+haingo-raz@users.noreply.github.com>
2025-02-08 04:10:53 +0000

    Add leetcode 75 day 19

---
+++ leetcode-75/day19/difference_of_two_arrays.py
@@ -0,0 +1,21 @@
+# https://leetcode.com/problems/find-the-difference-of-two-arrays/?envType=study-plan-v2&envId=leetcode-75
+
+class Solution(object):
+    def findDifference(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[List[int]]
+        """
+        s1, s2 = set(nums1), set(nums2)
+        ans = [[], []]
+
+        for i in s1:
+            if i not in s2:
+                ans[0].append(i)
+        
+        for i in s2:
+            if i not in s1:
+                ans[1].append(i)
+        
+        return ans
